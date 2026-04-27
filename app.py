85ee35a5c2fe2042f1ea916cdd39ab7dacdfd9a2
noouff4949-eng <noouff4949@gmail.com>
2026-04-28 02:16:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -288,10 +288,9 @@ def get_applicant_details(id):
 
 @app.route('/programs')
 def programs():
-    today = datetime.today().date()
-    smart_courses = Course.query.filter(Course.start_date_m >= today).order_by(Course.start_date_m.asc()).limit(20).all()
+    smart_courses = Course.query.all()
     return render_template('ShortPrograms.html', courses=smart_courses)
-
+    
 @app.route('/available')
 def available():
     workshops = AppliedWorkshop.query.all()
