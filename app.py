e50565ac4ac048fb3ad070fc09fb0efb18cd8afc
noouff4949-eng <noouff4949@gmail.com>
2026-04-27 23:19:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -530,6 +530,7 @@ def register_course():
         print(f"CRITICAL ERROR: {str(e)}")
         db.session.rollback(); return jsonify({"status": "error", "message": str(e)}), 500
 
-@app.before_request
-def create_tables():
-    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
